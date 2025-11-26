e5c8af76ea23776236c3e395b4d2bb0c29fa916e
mmcbbq <m.martinez@bbq.de>
2025-11-26 09:14:18 +0100

    service table done

--- public/assets/js/view/DownloadView.js
+++ public/assets/js/view/DownloadView.js
@@ -1,10 +1,10 @@
 import {AbstractView} from "./AbstractView.js";
 import {CustomerUploadForm} from "../element/CustomerUploadForm.js";
 
-export class DownloadView    extends AbstractView {
+export class DownloadView extends AbstractView {
     constructor(name) {
         super(name);
-        this.uploadForm = new CustomerUploadForm('.content', 'uploadservicefile', this,'./download/service/')
+        this.uploadForm = new CustomerUploadForm('.content', 'uploadservicefile', this, './download/service/')
         this.filedir = './download/service/'
     }
 
@@ -13,44 +13,53 @@ export class DownloadView    extends AbstractView {
         this.setContainerClasses(['content', 'container', "justify-content-center"])
         const data = await this.getFiles('getservicefiles')
         let color = '';
-        const rowContainer = document.createElement('div');
-        rowContainer.classList.add('row', 'row-cols-auto')
+
+        const tableElement = document.createElement('table');
+        tableElement.classList.add('table','table-hover')
+
+        const theadElement = document.createElement('thead');
+
+        // theadElement.classList.add('');
+
+
+        theadElement.innerHTML = [
+            '<tr>',
+                '<th scope="col" >Filename </th>',
+                '<th scope="col" >Beschreibung </th>',
+                '<th scope="col" >Von </th>',
+                '<th scope="col" >Download </th>',
+            '</tr>',
+        ].join('');
+
+        const tbodyEle = document.createElement('tbody');
+
+
+
         const filesCout = data.length;
         if (filesCout > 0) {
             for (let i = 0; i < filesCout; i++) {
-                const filesDiv = document.createElement('div');
-                filesDiv.classList.add('card', 'mx-2');
-                const filesBody = document.createElement('div');
-                filesBody.classList.add("card-body", 'm-0', 'p-1');
-                const fileTitle = document.createElement('h5');
-                fileTitle.classList.add('card-title', 'm-2');
-                fileTitle.innerText = data[i].name;
-                const fileText = document.createElement('p');
-                fileText.classList.add('m-0');
-                fileText.innerText = data[i].description;
-                const fileUser = document.createElement('p');
-                fileUser.innerText = data[i].email;
-                fileUser.classList.add('m-0');
-                const fileLink = document.createElement('a');
-                fileLink.href = this.filedir + data[i].name
-                fileLink.innerText = 'Download';
-                fileLink.classList.add('btn', 'btn-primary','btn-sm', 'mx-1');
-                fileLink.download = '';
+                const trbodyEle = document.createElement('tr');
+                trbodyEle.innerHTML = [
+                    `<td> ${data[i].name}</td>`,
+                    `<td> ${data[i].description}</td>`,
+                    `<td> ${data[i].email}</td> `,
+                    `<td><a href='${data[i].path}${data[i].name}' download ><button type="button" class="btn btn-primary">Download</button></td></a>`
+                ].join('');
 
-
-                filesBody.append(fileTitle, fileText, fileUser, fileLink);
-
-                filesDiv.appendChild(filesBody);
-                rowContainer.appendChild(filesDiv);
+                tbodyEle.append(trbodyEle);
 
             }
+            tableElement.appendChild(theadElement);
+            tableElement.appendChild(tbodyEle);
+            this.root.appendChild(tableElement);
+
+
         } else {
             const lineDiv = document.createElement('div');
             lineDiv.innerText = 'Keine Files';
             this.root.appendChild(lineDiv);
         }
-        this.root.appendChild(rowContainer)
-        if (this.loginstate.login && JSON.parse(this.loginstate.role).includes('admin') ){
+        if (this.loginstate.login && JSON.parse(this.loginstate.role).includes('admin')) {
             this.uploadForm.render()
         }
     }
